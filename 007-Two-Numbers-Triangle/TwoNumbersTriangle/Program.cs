1fe6094ae1fd4a54614caed9afa4ee81a91ea6bc
Daniel Lim-Apo <daniel.github@sede.com.br>
2024-01-09 00:56:31 -0300

    Two Numbers Triangle example

---
+++ 007-Two-Numbers-Triangle/TwoNumbersTriangle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+public class TwoNumbersTriangle
+{
+  public static void Main()
+  {
+   Console.Write("Input a number to be used: ");
+   int number = Convert.ToInt32( Console.ReadLine() );
+ 
+   Console.Write("Input the desired width for a triangle be done: ");
+   int width = Convert.ToInt32( Console.ReadLine() );
+ 
+   int height = width;   
+   for (int row=0; row < height; ++row)
+   {
+    for (int column=0; column < width; ++column)
+   {
+   Console.Write(number);
+  }
+ 
+   Console.WriteLine();
+   width--;
+  }
+ }
+} 
