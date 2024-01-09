1209b49e7e2d975901f3e52712d1bbca5b58c718
Daniel Lim-Apo <daniel.github@sede.com.br>
2024-01-09 01:42:35 -0300

    Cast Int To Short And Float To Int

---
+++ 010-Cast-Int-Short-and-Float-Int/CastIntToShortAndFloatToInt/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DataConversions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine();
+            // int to short (explicit cast)
+            int originalInt = 12345;
+            Console.WriteLine("originalInt is " + originalInt.GetType());
+            short convertedShort = (short)originalInt;
+            Console.WriteLine("convertedShort is " + convertedShort.GetType());
+            Console.WriteLine("int {0} converted to short {1}", originalInt, convertedShort);
+            Console.WriteLine();
+
+            // float to int (explicit cast)
+            float originalFloat = 3.14159f;
+            Console.WriteLine("originalFloat is " + originalFloat.GetType());
+            int convertedInt = (int)originalFloat;
+            Console.WriteLine("convertedInt is " + convertedInt.GetType());
+            Console.WriteLine("float {0} converted to int {1}", originalFloat, convertedInt);
+            Console.WriteLine();
+        }
+    }
+}
