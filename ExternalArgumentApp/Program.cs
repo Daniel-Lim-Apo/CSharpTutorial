1445f599c57c49d775eaca902745e72b84d4b558
Daniel Lim-Apo <daniel.github@sede.com.br>
2024-01-08 23:38:05 -0300

    External Argument example

---
+++ ExternalArgumentApp/Program.cs
@@ -0,0 +1,15 @@
+﻿class TestClass
+{
+    static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        Console.WriteLine(args.Length);
+
+        // Display all the string arguments.
+        for (int i = 0; i < args.Length; i++)
+        {
+            Console.WriteLine(args[i]);
+        }
+
+    }
+}
