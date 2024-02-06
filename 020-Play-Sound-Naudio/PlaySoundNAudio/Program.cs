7045298cabbdc87841ca02cb9bef3bf128b6ac69
Daniel Lim-Apo <daniel.github@sede.com.br>
2024-02-05 22:08:21 -0300

    Example Observer Pattern

---
+++ 020-Play-Sound-Naudio/PlaySoundNAudio/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using NAudio.Wave;
+
+public class Mp3Player
+{
+    private IWavePlayer wavePlayer;
+    private WaveStream waveStream;
+
+    public void PlayMp3FromResources()
+    {
+        // Substitua "YourNamespace" pelo namespace do seu projeto e "YourMp3ResourceName" pelo nome do seu recurso MP3
+        string resourceName = "PlaySoundNAudio.Resources.sound.mp3";
+        
+        // Carregando o recurso MP3
+        var assembly = Assembly.GetExecutingAssembly();
+        using (Stream resourceStream = assembly.GetManifestResourceStream(resourceName))
+        {
+            if (resourceStream == null)
+                throw new InvalidOperationException("Could not find embedded resource");
+
+            // Use NAudio para tocar o stream
+            waveStream = new Mp3FileReader(resourceStream);
+            wavePlayer = new WaveOutEvent();
+            wavePlayer.Init(waveStream);
+            wavePlayer.Play();
+        }
+    }
+
+    public void Stop()
+    {
+        wavePlayer?.Stop();
+        wavePlayer?.Dispose();
+        waveStream?.Dispose();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        var mp3Player = new Mp3Player();
+        mp3Player.PlayMp3FromResources();
+
+        Console.WriteLine("Press any key to stop...");
+        Console.ReadKey();
+
+        mp3Player.Stop();
+    }
+}
---
+++ 021-Observer-Pattern/ObserverPattern/Readme.md
@@ -0,0 +1,6 @@
+Conceptual Example
+
+From:
+https://refactoring.guru/design-patterns/observer/csharp/example
+
+Thanks for Refactoring Guru !
